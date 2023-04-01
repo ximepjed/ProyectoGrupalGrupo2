2511e4deb14c0d48a93e64209459177b10e31c1e
cchavesc318 <cchavesc318@ulacit.ed.cr>
2023-03-31 18:14:59 -0600

    holaa

---
+++ ProyectoGrupalGrupo2/Password_error.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoGrupalGrupo2
+{
+    /// <summary>
+    /// Lógica de interacción para Password_error.xaml
+    /// </summary>
+    public partial class Password_error : Window
+    {
+        public Password_error()
+        {
+            InitializeComponent();
+        }
+    }
+}
