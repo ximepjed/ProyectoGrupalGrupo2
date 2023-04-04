7df7bd475c8d64f6c3cb94b6eb211e0e050b55c7
Ximena Pérez Jackson <xperezj811@ulacit.ed.cr>
2023-04-04 14:17:07 -0600

    .

--- ProyectoGrupalGrupo2/Curso_clases/Notas.xaml.cs
+++ ProyectoGrupalGrupo2/Curso_clases/Notas.xaml.cs
@@ -43,18 +43,34 @@ namespace ProyectoGrupalGrupo2
 
         public void Button_Agregar1(object sender, RoutedEventArgs e)
         {// convertirlo en herencias y declarar para que no cambien solo por cerrar esta ventana 
-            int Grade1 = 0;
-            //Grade1 = Ingresar1.Text;
-            Nota1.Text = Ingresar1.Text;
+           
+           int Grade1;
+           Grade1 = int.Parse(Ingresar1.Text);
+            if (Grade1<0) { MessageBox.Show("La nota debe de estar entre 0 y 100"); }
+            if (Grade1 > 100) { MessageBox.Show("La nota debe de estar entre 0 y 100"); }
 
-            if (Grade1<70) { Estado1.Text= "Desaprobado"};
-            if (Grade1>=70) { Estado1.Text="Aprobado"};
+            else {
+                Nota1.Text = Ingresar1.Text;
+                if (Grade1 < 70) { Estado1.Text = "Desaprobado"; }
+                if (Grade1 >= 70) { Estado1.Text = "Aprobado"; }
+            }
+           
 
         }
 
         private void Button_Agregar2(object sender, RoutedEventArgs e)
-        {
-            Nota2.Text = Ingresar2.Text;
+        {// convertirlo en herencias y declarar para que no cambien solo por cerrar esta ventana 
+           
+            int Grade2;
+            Grade2 = int.Parse(Ingresar2.Text);
+            if (Grade2 > 100) { MessageBox.Show("La nota debe de estar entre 0 y 100"); }
+            if (Grade2 < 0) { MessageBox.Show("La nota debe de estar entre 0 y 100"); }
+
+            else {
+                Nota2.Text = Ingresar2.Text;
+                if (Grade2 < 70) { Estado2.Text = "Desaprobado"; }
+                if (Grade2 >= 70) { Estado2.Text = "Aprobado"; }
+            }
 
         }
     }
