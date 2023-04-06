e8f9c878e94ccc3d8c390bafd631705af15734c5
Ximena Pérez Jackson <xperezj811@ulacit.ed.cr>
2023-04-06 08:12:16 -0600

    ya en notas el textbox puede recibir los que sea sin errores y solo acepta numeros de 0 a 100 para las notas

--- ProyectoGrupalGrupo2/Curso_clases/Notas.xaml.cs
+++ ProyectoGrupalGrupo2/Curso_clases/Notas.xaml.cs
@@ -44,38 +44,45 @@ namespace ProyectoGrupalGrupo2
         public void Button_Agregar1(object sender, RoutedEventArgs e)
         {// convertirlo en herencias y declarar para que no cambien solo por cerrar esta ventana 
 
+            if (Ingresar1.Text.All(char.IsDigit)) {
                 int Grade1;
                 Grade1 = int.Parse(Ingresar1.Text);
                 if (Grade1 < 0) { MessageBox.Show("La nota debe de estar entre 0 y 100"); }
                 if (Grade1 > 100) { MessageBox.Show("La nota debe de estar entre 0 y 100"); }
 
-                    Nota1.Text = Ingresar1.Text;
-                    //variables para el cambio de color o brush
-                    var bc = new BrushConverter();
+                Nota1.Text = Ingresar1.Text;
+                //variables para el cambio de color o brush
+                var bc = new BrushConverter();
 
-                    if (Grade1 < 70) { Estado1.Text = "Reprobado"; Estado1.Background = bc.ConvertFrom("#FF7F2020") as Brush; }
-                    if (Grade1 >= 70) { Estado1.Text = "Aprobado"; Estado1.Background = bc.ConvertFrom("#FF6CB329") as Brush; }
+                if (Grade1 < 70) { Estado1.Text = "Reprobado"; Estado1.Background = bc.ConvertFrom("#FF7F2020") as Brush; }
+                if (Grade1 >= 70) { Estado1.Text = "Aprobado"; Estado1.Background = bc.ConvertFrom("#FF6CB329") as Brush; }
+            }
+
+            else { MessageBox.Show("La nota debe de estar entre 0 y 100"); }
+            
 
         }
 
         private void Button_Agregar2(object sender, RoutedEventArgs e)
         {// convertirlo en herencias y declarar para que no cambien solo por cerrar esta ventana 
            
+            if (Ingresar2.Text.All(char.IsDigit)) {
+                int Grade2;
+                Grade2 = int.Parse(Ingresar2.Text);
+                if (Grade2 < 0) { MessageBox.Show("La nota debe de estar entre 0 y 100"); }
+                if (Grade2 > 100) { MessageBox.Show("La nota debe de estar entre 0 y 100"); }
 
-            int Grade2;
-            Grade2 = int.Parse(Ingresar2.Text);
-            if (Grade2 < 0) { MessageBox.Show("La nota debe de estar entre 0 y 100"); }
-            if (Grade2 > 100) { MessageBox.Show("La nota debe de estar entre 0 y 100"); }
-
-            else
-                {
                 Nota2.Text = Ingresar2.Text;
-                    //variables para el cambio de color o brush
-                    var bc = new BrushConverter();
+                //variables para el cambio de color o brush
+                var bc = new BrushConverter();
 
-                    if (Grade2 < 70) { Estado2.Text = "Reprobado"; Estado2.Background = bc.ConvertFrom("#FF7F2020") as Brush; }
-                    if (Grade2 >= 70) { Estado2.Text = "Aprobado"; Estado2.Background = bc.ConvertFrom("#FF6CB329") as Brush; }
-                }
+                if (Grade2 < 70) { Estado2.Text = "Reprobado"; Estado2.Background = bc.ConvertFrom("#FF7F2020") as Brush; }
+                if (Grade2 >= 70) { Estado2.Text = "Aprobado"; Estado2.Background = bc.ConvertFrom("#FF6CB329") as Brush; }
+            }
+
+            else { MessageBox.Show("La nota debe de estar entre 0 y 100"); }
+
+
         }
     }
 }
