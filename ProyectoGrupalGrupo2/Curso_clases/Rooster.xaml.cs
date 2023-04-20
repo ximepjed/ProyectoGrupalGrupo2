ef74697474516ddebe66f429ee5b8b7b1b3944b8
Ximena Pérez Jackson <xperezj811@ulacit.ed.cr>
2023-04-20 07:56:48 -0600

    rooster

---
+++ ProyectoGrupalGrupo2/Curso_clases/Rooster.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoGrupalGrupo2.Curso_clases
+{
+    /// <summary>
+    /// Interaction logic for Rooster.xaml
+    /// </summary>
+    public partial class Rooster : Window
+    {
+        public Rooster()
+        {
+            InitializeComponent();
+        }
+    }
+}
