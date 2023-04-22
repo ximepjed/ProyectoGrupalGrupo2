dd9def75945d0889ea774b7e5d6988997af1287b
fmongec924 <114545502+fmongec924@users.noreply.github.com>
2023-04-21 19:06:18 -0600

    Agregue comentarios

--- ProyectoGrupalGrupo2/Curso_clases/Tareas.xaml.cs
+++ ProyectoGrupalGrupo2/Curso_clases/Tareas.xaml.cs
@@ -22,20 +22,18 @@ namespace ProyectoGrupalGrupo2
     public partial class Tareas : Window
     {
 
-        List<String> Categorias = new List<String>();
+        List<String> Categorias = new List<String>(); //Lista para guardar los elementos del combobox de categoria
         public static List<Datos_Tareas> Datos = new List<Datos_Tareas>(); //Guarda los datos de nombre, descripción, categoría y fecha
         public Tareas()
         {
             InitializeComponent();
             //Crea la lista de opciones para el combobox de categorias
-           
-
             Categorias.Add("Foro");
             Categorias.Add("Presentación");
             Categorias.Add("Asignación");
             Categorias.Add("Quiz");
 
-            cbox_Categoria.ItemsSource = Categorias;
+            cbox_Categoria.ItemsSource = Categorias; //Hca que combobox use los elementos creados anteriormente
 
 
 
@@ -49,17 +47,17 @@ namespace ProyectoGrupalGrupo2
 
         private void Button_agregar_tarea(object sender, RoutedEventArgs e)
         {
-            
+                //Si se cumplen las condiciones permite que se agreguen los elementos a la lista y que se muestre en el listbox
                 if (cbox_Categoria.SelectedItem != null && fecha_entrega_tarea.SelectedDate != null && txt_nombre_tarea.Text != null && txt_nombre_tarea.Text != " " && 
                 txt_descripcion_tarea.Text != null && txt_descripcion_tarea.Text != " " && txt_nombre_tarea.Text != string.Empty && txt_descripcion_tarea.Text != string.Empty)
                 {
 
-                    Llenar_Lista();
+                    Llenar_Lista(); //Llama al método que llena la lista "Datos"
 
-                    txt_nombre_tarea.Clear();
-                    txt_descripcion_tarea.Clear();
+                    txt_nombre_tarea.Clear();//Vacia el textbox del nombre
+                    txt_descripcion_tarea.Clear();//Vacia el textbox de la descripción
 
-                }
+            }
                 else
                 {
                 MessageBox.Show("Debe llenar todos los espacios.", "¡Falta informacion!", MessageBoxButton.OK, MessageBoxImage.Exclamation);
@@ -72,14 +70,18 @@ namespace ProyectoGrupalGrupo2
         {
             try
             {
+                //Asigna los valores de los textbox, combobox y select date a variables, para guardarlas en "Datos"
                 string nombre = txt_nombre_tarea.Text;
                 string categoria = cbox_Categoria.SelectedItem.ToString();
                 string descripcion = txt_descripcion_tarea.Text;
                 DateTime? Fecha = fecha_entrega_tarea.SelectedDate;
-                string fecha = Fecha.Value.ToString("dd/MM/yyyy"); //guarda la fecha seleccionada
+                string fecha = Fecha.Value.ToString("dd/MM/yyyy"); //guarda la fecha seleccionada tras volverla string
 
+                //Guardas las variables en la lista
                 Datos.Add(new Datos_Tareas($"{nombre}          ", $"|          {categoria}          |", $"           {descripcion}          |", $"          {fecha}"));
-                lb_Tareas.ItemsSource = null;
+               
+                //Refresca el listbox de tareas
+                lb_Tareas.ItemsSource = null; 
                 lb_Tareas.ItemsSource = Datos;
 
 
@@ -94,18 +96,18 @@ namespace ProyectoGrupalGrupo2
         {
             try
             {
-                if (txt_eliminar.Text.All(char.IsDigit))
+                if (txt_eliminar.Text.All(char.IsDigit)) //Permite que solo se ingresen numeros
                 {
                     int pos = int.Parse(txt_eliminar.Text);
                     if (pos > Datos.Count) { MessageBox.Show("Digite un número válido."); }
-                    Datos.RemoveAt(pos - 1);
-                    txt_eliminar.Clear();
+                    Datos.RemoveAt(pos - 1); //Elimina la posicion elegida por el usuario
+                    txt_eliminar.Clear(); //Vacia el textbox de eliminar
                 }
                 else { MessageBox.Show("Digite un número válido."); }
             }catch { }
          }
 
-            private void nombre_tarea_TextChanged(object sender, TextChangedEventArgs e)
+        private void nombre_tarea_TextChanged(object sender, TextChangedEventArgs e)
         {
            
         }
